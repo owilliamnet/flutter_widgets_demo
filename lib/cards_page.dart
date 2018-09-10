eeb44715dcb109d6cac93bcbc0b331ca00e68e29
William Martins <william@Note-William.house>
2018-09-10 13:24:56 -0300

    Finalizado app demonstração

--- lib/cards_page.dart
+++ lib/cards_page.dart
@@ -1,11 +1,106 @@
 import 'package:flutter/material.dart';
 
+class CardViagem {
+  final String titulo;
+  final List<String> descricao;
+
+  const CardViagem({
+    this.titulo,
+    this.descricao,
+  });
+}
+
+final List<CardViagem> destinos = <CardViagem>[
+  const CardViagem(
+    titulo: 'Rio de Janeiro',
+    descricao: const <String>[
+      'Corcovado',
+      'Cristo Redentor',
+      'Maracanã',
+    ],
+  ),
+  const CardViagem(titulo: 'São Paulo', descricao: <String>[
+    'Av. Paulista',
+    'Morumbi',
+    'Museu',
+  ]),
+  const CardViagem(titulo: 'Cuiaba', descricao: <String>[
+    'Orla do porto',
+    'Prainha',
+    'Fronteira',
+  ]),
+];
+
+class CardItens extends StatelessWidget {
+  CardItens({Key key, @required this.destinos, this.shape}) : super(key: key);
+
+  static const double height = 160.0;
+  final CardViagem destinos;
+  final ShapeBorder shape;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle descricaoStyle = theme.textTheme.subhead;
+
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Container(
+        padding: EdgeInsets.all(1.0),
+        height: height,
+        child: Card(
+          shape: shape,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                child: Text(
+                  destinos.titulo,
+                  style: descricaoStyle.copyWith(color: Colors.redAccent),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: DefaultTextStyle(
+                    softWrap: false,
+                    overflow: TextOverflow.ellipsis,
+                    style: descricaoStyle,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 1.0),
+                          child: Text(
+                            destinos.descricao[0],
+                            style: descricaoStyle.copyWith(color: Colors.green),
+                          ),
+                        ),
+                        Text(destinos.descricao[1]),
+                        Text(destinos.descricao[2]),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class CardsPage extends StatefulWidget {
   @override
   _CardsPageState createState() => _CardsPageState();
 }
 
 class _CardsPageState extends State<CardsPage> {
+  ShapeBorder _shape;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,9 +108,36 @@ class _CardsPageState extends State<CardsPage> {
         title: Text('Cards'),
         centerTitle: true,
         backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.sentiment_very_satisfied),
+            onPressed: () {
+              setState(() {
+                _shape = _shape != null
+                    ? null
+                    : const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(16.0),
+                          topRight: const Radius.circular(16.0),
+                          bottomLeft: const Radius.circular(2.0),
+                          bottomRight: const Radius.circular(2.0),
+                        ),
+                      );
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Cards'),
+      body: ListView(
+        itemExtent: CardItens.height,
+        padding:
+            const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0, bottom: 0.0),
+        children: destinos.map((CardViagem dest) {
+          return Container(
+            //margin: const EdgeInsets.only(bottom: 1.0),
+            child: CardItens(destinos: dest, shape: _shape),
+          );
+        }).toList(),
       ),
     );
   }
--- lib/listas_page.dart
+++ lib/listas_page.dart
@@ -1,62 +1,202 @@
 import 'package:flutter/material.dart';
 
+enum _MaterialListType {
+  umaLinha,
+  umLinhaComAvatar,
+  duasLinhas,
+  tresLinhas,
+}
+
 class ListasPage extends StatefulWidget {
+  const ListasPage({Key key}) : super(key: key);
+
   @override
   _ListasPageState createState() => _ListasPageState();
 }
 
 class _ListasPageState extends State<ListasPage> {
-  List<String> items = <String>[
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-  ];
-  bool _ordenacaoReversa = false;
+  static final GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
+  PersistentBottomSheetController<Null> _bottomSheet;
+  _MaterialListType _itemType = _MaterialListType.tresLinhas;
+  bool ordenacaoReversa = false;
+
+  bool _showAvatars = true;
+  bool _showIcons = false;
+
+  List<String> items = <String>['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
+  , 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'
+  , 'T', 'U', 'V', 'W', 'Y', 'X', 'Z'];
+  //bool _ordenacaoReversa = false;
+
+  void changeItemType(_MaterialListType type) {
+    setState(() {
+          _itemType = type;
+    });
+    _bottomSheet?.setState((){});
+  }
+
+  void _showConfigurationSheet() {
+
+    final PersistentBottomSheetController<Null> bottomSheet = 
+    scaffoldkey.currentState.showBottomSheet((BuildContext bottomSheetContext) {
+      return Container(
+        decoration: const BoxDecoration(
+          border: const Border(top: const BorderSide(color: Colors.black26)),
+          color: Colors.purpleAccent,
+        ),
+        child: ListView(
+          shrinkWrap: true,
+          primary: false,
+          children: <Widget>[
+            MergeSemantics(
+              child: ListTile(
+                dense: true,
+                title: Text('Uma Linha'),
+                trailing: Radio<_MaterialListType>(
+                  value: _MaterialListType.umaLinha,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                ),
+              ),
+            ),
+            MergeSemantics(
+              child: ListTile(
+                dense: true,
+                title: const Text('Duas Linhas'),
+                trailing: Radio<_MaterialListType>(
+                  value: _MaterialListType.duasLinhas,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                ),
+              ),
+            ),
+            MergeSemantics(
+              child: ListTile(
+                dense: true,
+                title: const Text('Três Linhas'),
+                trailing: Radio<_MaterialListType>(
+                  value: _MaterialListType.tresLinhas,
+                  groupValue: _itemType,
+                  onChanged: changeItemType,
+                ),
+              ),
+            ),
+
+            MergeSemantics(
+              child: ListTile(
+                dense: true,
+                title: const Text('Show Avatar'),
+                trailing: Checkbox(
+                  value: _showAvatars,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showAvatars = value;
+
+                    });
+                    _bottomSheet?.setState((){});
+                  }
+                ),
+              ),
+            ),
+
+            MergeSemantics(
+              child: ListTile(
+                dense: true,
+                title: const Text('Show Ícone'),
+                trailing: Checkbox(
+                  value: _showIcons,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _showIcons = value;
+
+                    });
+                    _bottomSheet?.setState((){});
+                  }
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+
+    setState(() {
+      _bottomSheet = bottomSheet;
+    });
+
+
+    _bottomSheet.closed.whenComplete((){
+      if (mounted) {
+        setState(() {
+          _bottomSheet = null;
+        });
+      }
+    });
+  }
+
+  Widget buildListTile(BuildContext context, String item) {
+    Widget secondary;
+    if (_itemType == _MaterialListType.duasLinhas) {
+      secondary = const Text('Texto secundário');
+    } else if (_itemType == _MaterialListType.tresLinhas) {
+      secondary = Text('Essa linha adicional aparece com o modelo 3 linhas. Vou adicionar mais texto aqui para quebrar a terceira linha...',
+      );
+    }
+    return MergeSemantics(
+      child: ListTile(
+        isThreeLine: _itemType == _MaterialListType.tresLinhas,
+        leading: _showAvatars ? ExcludeSemantics(child: CircleAvatar(child: Text(item))): null,
+        trailing: _showIcons ? Icon(Icons.info, color: Theme.of(context).disabledColor): null,
+        title: Text('Esse item representa a letra $item.'),
+        subtitle: secondary,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    Iterable<Widget> listTiles =
-        items.map((String item) => buildListTile(context, item));
+    String itemTypeText;
+    switch (_itemType) {
+      case _MaterialListType.umaLinha:
+      case _MaterialListType.umLinhaComAvatar:
+        itemTypeText = 'Uma Linha';
+        break;
+      case _MaterialListType.duasLinhas:
+        itemTypeText = 'Duas Linhas';
+        break;
+      case _MaterialListType.tresLinhas:
+        itemTypeText = 'Três Linhas';
+        break;
+    }
+
+    Iterable<Widget> listTiles = items.map((String item) => buildListTile(context, item));
+    listTiles = ListTile.divideTiles(context: context, tiles: listTiles);
 
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
-        backgroundColor: Colors.indigoAccent,
         title: Text('Listas Page'),
-        centerTitle: true,
+        backgroundColor: Colors.red,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.sort_by_alpha),
+            icon: const Icon(Icons.sort_by_alpha),
+            tooltip: 'Sort',
             onPressed: () {
               setState(() {
-                _ordenacaoReversa = !_ordenacaoReversa;
-                items.sort((String a, String b) => _ordenacaoReversa ? b.compareTo(a) : a.compareTo(b));
+                ordenacaoReversa = !ordenacaoReversa;
+                items.sort((String a, String b) => ordenacaoReversa ? b.compareTo(a) : a.compareTo(b));
               });
             },
           ),
           IconButton(
             icon: Icon(Icons.more_vert),
-            onPressed: null,
-          )
+            onPressed: _bottomSheet == null ? _showConfigurationSheet: null,
+          ),
         ],
       ),
       body: Scrollbar(
         child: ListView(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
           children: listTiles.toList(),
         ),
       ),
@@ -64,13 +204,4 @@ class _ListasPageState extends State<ListasPage> {
   }
 }
 
-Widget buildListTile(BuildContext context, String item) {
-  Widget secondary = const Text('Texto secundário');
 
-  return MergeSemantics(
-    child: ListTile(
-      title: Text('Este item representa a letra $item'),
-      subtitle: secondary,
-    ),
-  );
-}
